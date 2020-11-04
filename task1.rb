e610c90fa85c8f84c1a1314a0198d6baad9b3a4c
tenelles <kirilmorozov.k@yandex.ru>
2020-11-04 16:58:39 +0300

    Update task1.rb

--- task1.rb
+++ task1.rb
@@ -1,31 +1,27 @@
 class Station
-  attr_reader :trains_list # Из ТЗ
-  attr_reader :name        # Для вывода станций в методе класса Route
+  attr_reader :trains_list
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @trains_list = []
   end
 
-  # Принятие одного поезда на стацию
   def take_train(train)
     trains_list << train
   end
 
-  # Отправка конкретного поезда со станции, если таковой на ней имеется
   def send_train(train)
     trains_list.delete(train)
   end
   
-  # Список поездов с указанным типом
-  def trains_list_by_type(type) # 
+  def trains_list_by_type(type) 
     trains_list.select { |train| train.type == type }
   end
 
 end
 
 class Route
-  #Для комфортной работы класса Train
   attr_reader :start_station
   attr_reader :finish_station
 
@@ -35,12 +31,10 @@ class Route
     @intermediate_stations = []
   end
   
-  # Добавление указанной промежуточной станции
   def add_station(station)
     intermediate_stations << station
   end
 
-  # Удаление указанной станции из списка промежуточных, если она там имеется
   def delete_station(station)
     intermediate_stations.delete(station)
   end
@@ -48,19 +42,12 @@ class Route
   def stations
     [start_station, *@intermediate_stations, finish_station]
   end
-
-  # Вывод всех станций по порядку следования
-  def print
-    puts "^ #{start_station.name}"
-    intermediate_stations.each { |station| puts "! #{station.name}" }
-    puts "- #{finish_station.name}"
-  end
 end
 
 class Train
-  attr_reader :speed # По ТЗ
-  attr_reader :type # Для работы методов класса Station
-  attr_reader :vans_count # По ТЗ
+  attr_reader :speed
+  attr_reader :type
+  attr_reader :vans_count
 
   def initialize(number, type, vans_count)
     @number = number
@@ -69,78 +56,60 @@ class Train
     @speed = 0
   end
 
-  # Увеличить скорость на value
   def increase_speed(value)
     @speed += value
   end
 
-  # Обнулить скорость
   def stop
     @speed = 0
   end
 
-  # Увеличить кол-во вагонов, если поезд стоит
   def attach_van
     @vans_count += 1 if speed == 0
   end
 
-  # Уменьшить кол-во вагонов, если поезд стоит
   def detach_van
     @vans_count -= 1 if speed == 0 && vans_count > 0
   end
 
-  # Переключиться на указанный маршрут
   def choose_route(chosen_route)
     @route = chosen_route
     @station = @route.stations[0]
     @station.take_train(self)
   end
   
-  # Продвинуться по маршруту вперед, если не в конце пути
-  def move_forward_on_route
-    unless @route.nil?
-      station_index = @route.stations.index(@station)
-      station_index = 0 if station_index.nil? # Обработка удаления из маршрута станции, на которой есть поезд, следующий по данному маршруту
-      unless @station == @route.finish_station
-        @route.stations[station_index].send_train(self)
-        station_index += 1
-        @station = @route.stations[station_index]
-        @route.stations[station_index].take_train(self)
-      end
-    end
+  def move_forward
+    return if @route.nil?
+    @station.send_train(self)
+    @station = next_station
+    @station.take_train(self)
   end
 
-  # Продвинуться по маршруту назад, если не в начале пути
-  def move_back_on_route
-    unless @route.nil?
-      station_index = @route.stations.index(station)
-      station_index = 0 if station_index.nil? # Обработка удаления из маршрута станции, на которой есть поезд, следующий по данному маршруту
-      unless @station == @route.start_station
-        @route.stations[station_index].send_train(self)
-        station_index -= 1
-        @station = @route.stations[station_index]
-        @route.stations[station_index].take_train(self)
-      end
-    end
+  def move_back
+    return if @route.nil?
+    @station.send_train(self)
+    @station = prev_station
+    @station.take_train(self)
   end
 
-  def current_station_on_route
-    @station unless @route.nil?
+  def current_station
+    @station if route
   end
 
-  def next_station_on_route
-	unless @route.nil?
-	  station_index = @route.stations.index(station)
-      station_index = 0 if station_index.nil?
-      @route[station_index + 1] unless station_index == @route.size - 1
-    end
+
+  def next_station
+	  return if @route.nil?
+	  station_index = @route.stations.index(@station)
+    station_index = 0 if station_index.nil?
+    return if station_index == @route.stations.size - 1
+    @route.stations[station_index + 1]
   end
 
-  def prev_station_on_route
-	unless @route.nil?
-	  station_index = @route.stations.index(station)
-      station_index = 0 if station_index.nil?
-      @route[station_index - 1] unless station_index == 0
-    end
+  def prev_station
+  	return if @route.nil?
+	  station_index = @route.stations.index(@station)
+    station_index = @route.stations.size - 1 if station_index.nil?
+    return if station_index == 0
+    @route.stations[station_index - 1]
   end
 end
