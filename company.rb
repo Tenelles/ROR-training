476d60dc9bfaeb52a770c66d3c1205c96f138120
tenelles <kirilmorozov.k@yandex.ru>
2020-11-06 23:01:34 +0300

    Trying to fix problem

--- company.rb
+++
@@ -1,3 +0,0 @@
-module Company
-  attr_accessor :set_company_name
-end
--- vans/passenger_van.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require_relative 'van'
-
-class PassengerVan < Van
-  include InstanceCounter
-  def type # public, как у родителя
-    :passenger
-  end
-end
