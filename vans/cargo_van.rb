f67011be53a4ea6f5171d61a90104442a473af42
tenelles <tenelles@yandex.ru>
2020-11-10 21:53:40 +0300

    Добавлен функционал для вагонов

--- vans/cargo_van.rb
+++ vans/cargo_van.rb
@@ -2,9 +2,51 @@
 
 require_relative 'van'
 
+=begin
+	Список ошибок:
+		fill_volume: "Недостаточно свободного объема" - попытка занять объем, не имея достаточно свободного объема
+		validate!: "Отрицательный объем"
+=end
+
 class CargoVan < Van
   include InstanceCounter
+
+  attr_reader :volume
+  attr_reader :filled_volume
+
+  def initialize(volume)
+  	@volume = volume
+  	filled_volume = 0
+  	validate!
+  end
+
+  def valid?
+  	validate!
+  	true
+  rescue RuntimeError
+  	false
+  end
+
   def type
     :cargo
   end
+
+  def fill_volume(volume)
+  	raise 'Недостаточно свободного объема' if volume > free_volume
+  	filled_volume += volume
+  end
+
+  def free_volume
+  	volume - filled_volume
+  end
+
+  protected
+
+  attr_writer :volume
+  attr_writer :filled_volume
+
+  def validate!
+  	raise 'Отрицательный объем' if volume < 0
+  end
+
 end
--- vans/passenger_van.rb
+++ vans/passenger_van.rb
@@ -2,9 +2,49 @@
 
 require_relative 'van'
 
+=begin
+	Список ошибок:
+		take_place: "Недостаточно свободных мест" - попытка занять место, когда все занято
+		validate: "Отрицательное число мест"
+=end
+
 class PassengerVan < Van
   include InstanceCounter
+
+  attr_reader :places_count
+  attr_reader :free_places_count
+
+  def initialize(count)
+  	@places_count = count;
+  	@free_places_count = count;
+  	validate!
+  end
+
+  def valid?
+  	validate!
+  	true
+  rescue RuntimeError
+  	false
+  end
+
   def type
     :passenger
   end
+
+  def take_place
+  	raise "Недостаточно свободных мест" if free_places_count <= 0
+  	self.free_places_count -= 1;
+  end
+
+  def taken_places_count
+  	places_count - free_places_count;
+  end
+
+  protected
+
+  def validate!
+  	raise "Отрицательное число мест" if places_count < 0;
+
+  attr_writer :places_count
+  attr_writer :free_places_count
 end
